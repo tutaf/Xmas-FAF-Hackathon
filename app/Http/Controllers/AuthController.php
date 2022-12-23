7c467209747b43c4f252291e2167758f798742c3
Eduard <dz6class@mail.ru>
2022-12-23 22:46:57 +0200

    Update AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,6 +26,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
+            'switcher' => 'required'
         ]);
 
         if ($validator->fails()) {
@@ -37,7 +38,8 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
-            'access_token' => $token
+            'access_token' => $token,
+            'type' => $request->switcher == 1 ? 1 : 0
         ]);
 
         $user->save();
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'access_token'
+        'access_token',
+        'type'
     ];
 
     /**
